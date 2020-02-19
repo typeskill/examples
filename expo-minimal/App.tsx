fe284c9bc122fc995718c63f0a5ad038260c6e8d
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-02-19 13:19:35 -0300

    chore(deps): upgrade to expo SDK 36

--- expo-minimal/App.tsx
+++ expo-minimal/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { YellowBox } from 'react-native'
 import { Editor } from './src/Editor'
 
 declare const global: any
+
+YellowBox.ignoreWarnings(['Icon'])
 
 // @see: https://github.com/facebook/react-native/issues/9599
 if (typeof (global as any).self === 'undefined') {
